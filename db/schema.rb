4e48072079cce03cf2dc5503f6abc53b1569fb50
tirempbs <tirem@fastmail.com>
2019-06-03 19:10:26 -0400

    Changed database from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_23_203518) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
     t.integer "recipient_count"
